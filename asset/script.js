686c99c886bdca1eb3ebe445ebc1c75968360267
Fatih <fatihce57@gmail.com>
2021-10-07 16:55:19 +0200

    Precise face recognition setting has been made

--- asset/script.js
+++ asset/script.js
@@ -1,6 +1,7 @@
 const video = document.getElementById('video');
 const debug = document.getElementById('debug');
-const virus = new Image()
+const virus = new Image();
+const facePoint = [];
 
 ///////////CAMERA STARTED/////////
 Promise.all(
@@ -22,12 +23,24 @@ Promise.all(
 
 
 /////////FACE RECOGNITION////////////////
+
+function average(array) {
+    var sum = 0;
+    for (var i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+    return sum / array.length
+}
+
+
+
+
 video.addEventListener('play', () => {
     console.log('video started');
 
     const canvas = faceapi.createCanvasFromMedia(video);
     const context = canvas.getContext('2d');
-    ocument.body.append(canvas);
+    document.body.append(canvas);
 
     const displaySize = { width: canvas.width, height: canvas.height };
     faceapi.matchDimensions(canvas, displaySize);
@@ -36,10 +49,29 @@ video.addEventListener('play', () => {
     setInterval(async () => {
         const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions());
         context.clearRect(0, 0, canvas.width, canvas.height);
-        detections.forEach(face => {
-            debug.innerHTML = face._score;
-            console.log(face);
+
+        detections.forEach((face, i) => {
+
+
+
+            let ar = facePoint[i] || new Array();
+            ar.push(face._score);
+            if (ar.length > 5) {
+                ar.shift();
+            }
+            facePoint[i] = ar;
+            debug.innerHTML = average(facePoint[i]);
+
+            if (average(facePoint[i]) > .85) {
+                context.drawImage(virus, face._box._x, face._box._y, face._box._width, face._box._height);
+            }    
         });
+
+
+
+
+
+
     }, 100);
 
 
